76e0d06c1cf6386c1793d6b990010eb377c3cddb
yoko0180 <yoko0180@gmail.com>
2023-06-24 21:55:24 +0900

    tidy

--- main_test.ts
+++ main_test.ts
@@ -1,30 +1,14 @@
 import { assertEquals } from "https://deno.land/std@0.192.0/testing/asserts.ts"
 import { fetchTransform } from "./fetchTransform.ts";
 
-/**
- * 関数内で指定されたwritableをクローズするかしないか
- * 
- * 利用側でopenするため利用側でクローズするのが望ましいのかもしれない
- * テストでstdoutを使うとテストメソッド外で開かれているものがテスト内でクローズしないでくださいというエラーになることも裏付けているのかもしれない。
- */
-
-
-
-Deno.test("fetchTransform stdout test", async () => {
-  const context = { name: "fooooooo3" }
-  const url = "https://github.com/yoko0180/deno-handlebars/raw/master/template.hbs"
-  const w = Deno.stdout.writable
-  await fetchTransform(url, w, context)
-  // assertEquals(add(2, 3), 5)
-})
-
-Deno.test("fetchTransform file out test", async () => {
+Deno.test("fetchTransform test", async () => {
   const TEST_OUT = "test-out.txt"
   const outfile = await Deno.open(TEST_OUT, { create: true, write: true })
-  const context = { name: "fooooooo3" }
+  const context = { name: "foo" }
   const url = "https://github.com/yoko0180/deno-handlebars/raw/master/template.hbs"
   const w = outfile.writable
   await fetchTransform(url, w, context)
   w.close()
+  assertEquals(Deno.readTextFileSync(TEST_OUT), "hello foo")
   await Deno.remove(TEST_OUT)
 })
